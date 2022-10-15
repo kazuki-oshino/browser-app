07cb6612403d7cb5c2e5d050aea08beb66c18d3e
kazuikios <slash0juice@gmail.com>
2022-10-15 18:08:54 +0900

    feat: use localstorage

--- ts/TaskCollection.ts
+++ ts/TaskCollection.ts
@@ -1,15 +1,24 @@
 import {Status, Task} from "./Task";
 
+const STORAGE_KEY = 'TASKS'
 
 export class TaskCollection {
+  private readonly storage
   private tasks: Task[] = []
+
+  constructor() {
+    this.storage = localStorage
+    this.tasks = this.getStoredTasks()
+  }
 
   add(task: Task) {
     this.tasks.push(task)
+    this.updateStorage()
   }
 
   delete(task: Task) {
     this.tasks = this.tasks.filter(({id}) => id !== task.id)
+    this.updateStorage()
   }
 
   find(id: string) {
@@ -27,4 +36,21 @@ export class TaskCollection {
     return this.tasks.filter(({status}) => status === filterStatus)
   }
 
+  private updateStorage() {
+    this.storage.setItem(STORAGE_KEY, JSON.stringify(this.tasks))
+  }
+
+  private getStoredTasks() {
+    const jsonString = this.storage.getItem(STORAGE_KEY)
+    if (!jsonString) return []
+    try {
+      const storedTasks: any[] = JSON.parse(jsonString)
+      const tasks = storedTasks.map((task) => new Task(task))
+      console.log(tasks)
+      return tasks
+    } catch {
+      this.storage.removeItem(STORAGE_KEY)
+      return []
+    }
+  }
 }
